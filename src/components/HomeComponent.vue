e456d90b5a7aa7e09c7214949e9252bf50773fbf
Xaykham <xaykham@Xaykhams-MacBook-Pro.local>
2024-10-13 17:51:08 +0700

    Update content and yml

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -23,7 +23,7 @@
             <div class="text">
               <p>
                 I'm creative designer based in Vientiane, and I'm very passionate
-                and dedicated to my work at LaoVietBank over 10 years.
+                and dedicated to my work at LaoVietBank
               </p>
             </div>
             <div class="button">
